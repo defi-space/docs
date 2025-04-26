694c0ec6263f62ca2881950733d3b4f83af53e5d
¡nteractif [•] syntheªº <202481869+defidotspace@users.noreply.github.com>
2025-04-25 22:18:32 -0400

    chore: Refactor Docusaurus configuration and package scripts. Updated baseUrl logic to use double quotes and modified deploy scripts to utilize environment variables for Google Tag Manager ID.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,8 +6,7 @@ const math = require("remark-math");
 const katex = require("rehype-katex");
 require("dotenv").config();
 
-
-const isDeployPreview = process.env.DEPLOY === 'true';
+const isDeployPreview = process.env.DEPLOY === "true";
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
@@ -19,7 +18,7 @@ const config: Config = {
   url: "https://defi-space.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: isDeployPreview ? '/docs/' : '/',
+  baseUrl: isDeployPreview ? "/docs/" : "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -62,7 +61,7 @@ const config: Config = {
           customCss: "./src/css/custom.css",
         },
         googleTagManager: {
-          containerId: "GTM",
+          containerId: process.env.GOOGLE_TAG_MANAGER_ID || "",
         },
       } satisfies Preset.Options,
     ],
